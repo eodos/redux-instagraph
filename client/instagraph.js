9df6bdc3128ed4cc8a1967ba1811b8ba6256fbb2
David Paul Pena <dppena@magicleap.com>
2017-07-30 22:17:19 -0700

    Add store and actions

--- client/instagraph.js
+++ client/instagraph.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
+
+// import style
 import css from './styles/style.styl';
+
+// import components
 import Main from './components/Main';
 import Single from './components/Single';
 import PhotoGrid from './components/PhotoGrid';
+
+// import router
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 const router = (
