79707b66d94e3081ba6a4be2144af18944e75fdb
David Paul Pena <dppena@magicleap.com>
2017-07-30 22:32:49 -0700

    Add empty reducers

---
+++ client/store.js
@@ -0,0 +1,22 @@
+import { createStore, compose } from 'redux';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { browserHistory } from 'react-router';
+
+// import reducers
+import { rootReducer } from './reducers/index';
+
+// import sample data
+import comments from './data/comments.js';
+import posts from './data/posts.js';
+
+// create object for sample data
+const defaultState = {
+	posts,
+	comments
+};
+
+store = createStore(rootReducer, defaultState);
+
+export const history = syncHistoryWithStore(browserHistory, store);
+
+export default store;
